18b1b4c171d2571aa84dcb6d3533a1c92b826d8a
mateo1761 <308@academicos.cesde.edu.co>
2022-03-03 07:39:11 -0500

    Se creo for y array

--- reto.py
+++ reto.py
@@ -1,3 +1,4 @@
+'''
 contador = 1
 sumatoria = 0
 
@@ -8,3 +9,22 @@ while(contador <= 5):
         sumatoria += 1
 
 print(f'La cantidad de numeros negativos es {sumatoria}')
+'''
+
+##Ciclo for
+
+##for i in range(1,20,5):
+##    print(i)
+
+##trabajos con Arreglos (LISTAS)
+
+nombres = [
+    "juan",
+    "carlos",
+    "marta"
+]
+print(nombres)
+
+#for para recorrer listas en PY
+for nombre in nombres:
+    print(nombre)
